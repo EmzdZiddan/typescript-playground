2723c4ff0221f7624639c58bfb4d64f443dd9079
EmzdZiddan <emzdziddan@gmail.com>
2026-02-20 12:47:23 +0700

    chore: add config files

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const nama = "Dimas";
+const umur = 22;
+console.log(`Hello, ${nama}! Umur kamu adalah ${umur} tahun.`);
+//# sourceMappingURL=index.js.map
